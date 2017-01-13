48384847e77b089bb456563ee341143c689577de
lopus-mopus <vchorasao@mail.ru>
2017-01-13 12:26:46 +0700

    до

--- lab3/src/lab3/Constants.java
+++
@@ -1,8 +0,0 @@
-package lab3;
-
-class Constants {
-    static final int TIMEOUT = 50;
-    static final int MAX_SIZE = 1024;
-    static final int QUEUE_MAX_CAPACITY = 500;
-    static final int ACCEPTED_COUNT = 100;
-}
--- lab3/src/lab3/WrongNodeException.java
+++
@@ -1,7 +0,0 @@
-package lab3;
-
-import java.io.IOException;
-
-public class WrongNodeException extends IOException {
-    public WrongNodeException(String msg) { super(msg); }
-}
